517b8fe3bf6ed2a373f90a1e653a8b4ebf283f00
stianrincon <stianrincon@outlook.com>
2013-03-05 13:09:21 -0800

    primeros pasos con el login

--- CodeSchool/CodeSchool/urls.py
+++ CodeSchool/CodeSchool/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 from CodeSchool import settings
 
 urlpatterns = patterns('',
-    url(r'^$','WebSchool.views.login'),
+    url(r'^$','WebSchool.views.login_school'),
+    url(r'^welcome_administrator/$','WebSchool.views.welcome_administrator'),
     url(r'^base_administrator/$','WebSchool.views.base_administrator'),
     url(r'^qualification/$','WebSchool.views.qualification'),
     url(r'^media/(?P<path>.*)$','django.views.static.serve',
--- CodeSchool/WebSchool/views.py
+++ CodeSchool/WebSchool/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 
-def login(request):
+def login_school(request):
+    if not request.user.is_anonymous():
+        return HttpResponseRedirect('/')
     if request.method == 'POST':
-        return render_to_response('welcome_administrator.html', context_instance = RequestContext(request))
+        userName = request.POST['userName']
+        userPassword = request.POST['userPassword']
+        access = authenticate(username=userName, password=userPassword)
+        if access is not None:
+            if access.is_active:
+                login(request, access)
+                return HttpResponseRedirect('/welcome_administrator')
+            else:
+                return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/')
     else: 
         return render_to_response('login.html', context_instance = RequestContext(request))
-    
+
+def welcome_administrator(request):
+    return render_to_response('welcome_administrator.html', context_instance = RequestContext(request))
+
 def base_administrator(request):
     return render_to_response('base_administrator.html',{'base_administrator':base_administrator}, context_instance=RequestContext(request))
 
